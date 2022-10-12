36efbf3b19d4bd14e7d1dcd880e1e5f9f5d6db9a
Henrique Nery <henriquenery07@gmail.com>
2022-10-12 12:22:04 -0300

    feat: add homeHandler function

--- main.go
+++ main.go
@@ -3,5 +3,10 @@ package main
 import "net/http"
 
 func main() {
+	http.HandleFunc("/", homeHandler)
 	http.ListenAndServe(":3000", nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
 }
