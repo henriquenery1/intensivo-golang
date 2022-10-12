3818b18f54999f4ae155f856f555b4c119e6cd90
Henrique Nery <henriquenery07@gmail.com>
2022-10-12 12:33:37 -0300

    feat: product and function SaveProduct on db

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"database/sql"
+	"net/http"
+)
 
 type Product struct {
 	ID string
@@ -9,10 +12,24 @@ type Product struct {
 }
 
 func main() {
+	pruduct := Product {
+		ID: "1",
+		Name: "Product 1",
+		Price: 100.00,
+	}
+	println(pruduct.ID)
+
 	http.HandleFunc("/", homeHandler)
 	http.ListenAndServe(":3000", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world!"))
+}
+
+func SaveProduct(product Product) {
+	db, err := sql.Open("sqlite3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
 }
